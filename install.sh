3e5780950af50324355e8c484615fe83a15b8924
dominicmortlock <dmortlock@lyft.com>
2017-06-07 14:26:22 -0700

    Edit dot files for more vim stuff

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@ set -e
 
 echo "Installing dotfiles..."
 
+# Set bash profile
+cat ./bash_profile > ~/.bash_profile
+
+# Add vimrcs to vimrc
+cat ./vimrcs/personal.vim > ./vimrc
+cat ./vimrcs/basic.vim >> ./vimrc
+
 # Symlink vimrc to ~/.vimrc
 ln -sfv ~/src/dotfiles/vimrc ~/.vimrc
