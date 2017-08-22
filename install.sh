8f04f8f4d8daf100582f338470fa98f9f296551b
Dominic Mortlock <dom.mortlock@gmail.com>
2017-08-22 03:19:28 -0700

    Explore neovim settings

--- install.sh
+++ install.sh
@@ -7,11 +7,15 @@ echo "Installing dotfiles..."
 cat ./configs/bash_profile > ~/.bash_profile
 
 # Add vimrcs to vimrc
-cat ./configs/vimrcs/personal.vim > ./vimrc
-cat ./configs/vimrcs/basic.vim >> ./vimrc
+cat ./configs/vimrcs/personal.vim > ./configs/vimrc
+cat ./configs/vimrcs/zack.vim >> ./configs/vimrc
 
 # Symlink vimrc to ~/.vimrc
 ln -sfv ./configs/vimrc ~/.vimrc
+
+# Symlink init.vim to ~/.config/nvim/init.vim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config/nvim}
+ln -sfv /configs/vimrc $XDG_CONFIG_HOME/init.vim
 
 # Install brew files
 sh ./install/brew.sh
